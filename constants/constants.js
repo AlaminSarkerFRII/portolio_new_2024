e10966952221fff5d151503d9c9925725f13b52a
AlaminSarkerFRII <alamin.education1997@gmail.com>
2024-05-17 11:09:24 +0600

    setup initals projects

---
+++ constants/constants.js
@@ -0,0 +1,71 @@
+export const NAME = 'Alamin Sarker';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'Bangadesh',
+  City: 'Dhaka',
+  Age: '27',
+};
+
+export const SKILLS = [
+  {
+    title: 'Software Engineer',
+    level: '90%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Full Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'Python Django Developer',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'Python',
+  'TypeScript',
+  'ReactJS',
+  'NextJS',
+  'Docker',
+  'Django',
+  'Django Rest Framework',
+  'Celery',
+  'Webpack',
+  'Postrgesql',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+];
+
+export const CONTACTS = {
+  EMAIL: 'alamin.sarker4241@gmail.com',
+  PHONE: '+880-1740051568',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/AlaminSarkerFRII',
+  LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
